8cfa5797f627e3cf796e7de8bf37fa869425e654
Archie Fan <archiefan@gmail.com>
2022-05-24 19:12:13 +0100

    Add project files.

---
+++ extensionDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "Hello extension Method?";
+            int iq = s.WordCount();
+            Console.WriteLine($"Number of Word: {iq} ");
+            Console.ReadLine();
+        }
+    }
+}
